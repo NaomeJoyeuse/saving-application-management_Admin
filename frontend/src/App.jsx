12055596d13a9e0ebe0fb67ddd90ce783f2edce5
NaomeJoyeuse <naomejoyce5@gmail.com>
2025-10-30 20:13:10 +0200

    frontend initiation

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+
+  export default function App() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">Tailwind is working 🚀</h1>
+      <button className="mt-6 bg-blue-600 text-white px-4 py-2 rounded-lg hover:bg-blue-700">
+        Test Button
+      </button>
+    </div>
+  );
+}
+
+
